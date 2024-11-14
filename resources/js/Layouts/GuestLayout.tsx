5a0fc1669cabb25ab223de69e9a4701547c223bf
miftt <miftahfauzi012@gmail.com>
2024-11-14 15:12:04 +0700

    fix bg

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <div className="flex min-h-screen flex-col items-center hero-bg pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
                 <Link href="/">
                     <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
