9c76d43abe164c2f8373537666d93fa5b92793c3
miftt <miftahfauzi012@gmail.com>
2024-11-24 21:50:30 +0700

    feat: type for movie

---
+++ resources/js/types/movie.ts
@@ -0,0 +1,12 @@
+interface Movie {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+    vote_average: number;
+    release_date: string;
+    genre_ids: number[];
+}
+
+export type { Movie };
