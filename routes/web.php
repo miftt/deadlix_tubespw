c175ed8cc27f1d176dd1cf8342760f9943a46e50
miftt <miftahfauzi012@gmail.com>
2024-12-03 23:36:26 +0700

    feat(route): add route movie details with slug #8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieDetailsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -26,5 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/movie/{id}', [MovieDetailsController::class, 'index'])->name('moviedetails.movie');
 
 require __DIR__ . '/auth.php';
