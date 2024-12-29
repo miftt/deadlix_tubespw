0b71305415064838b5dbc237006a009d98096af5
Akbarni <rediakbar082@gmail.com>
2024-12-29 19:31:03 +0700

    feat: add controller search movie #11

---
+++ app/Http/Controllers/MovieSearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TMDBService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class MovieSearchController extends Controller
+{
+    private $tmdbService;
+
+    public function __construct(TMDBService $tmdbService)
+    {
+        $this->tmdbService = $tmdbService;
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('query');
+
+        if (empty($search)) {
+            return response()->json([
+                'results' => []
+            ]);
+        }
+
+        $results = $this->tmdbService->searchMovies($search);
+
+        Log::info('Search results controller', [
+            'results' => $results
+        ]);
+
+        return response()->json([
+            'results' => $results['results']
+        ]);
+    }
+}
