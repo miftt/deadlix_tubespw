a8a8f3dc8f121b6ac5d1f65a8082e6dc36c68b8a
Akbarni <rediakbar082@gmail.com>
2024-12-31 19:23:42 +0700

    feat: add watchlist functionality #18

---
+++ app/Models/Watchlists.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Watchlists extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'movie_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
